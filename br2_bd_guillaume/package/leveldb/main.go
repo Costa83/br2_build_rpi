e1f424ea4234ec66da45d599e419ad36917b5c69
Costa83 <guillaume.costagliola@gmail.com>
2020-06-05 23:42:59 +0200

    Add files via upload

--- br2_bd_guillaume/package/leveldb/main.go
+++ br2_bd_guillaume/package/leveldb/main.go
@@ -17,12 +17,15 @@ func main() {
 
 
   path := "testleveldb"
-  lignes := "1"
-  rows := "1000000"
+  rows := "7"
+  print := "true"
+  nb_row_to_print := "6 Source"
+  nb_row_to_delete := "7 Source"
+  
 
   fmt.Printf("PROGRAMME DE TEST DE LEVELDB EN GO")
   fmt.Printf("## path : %s",path)
-  fmt.Printf("## lignes : %s",lignes)
+  fmt.Printf("## lignes : %s",rows)
  
 
   /*Création de la base de donnée*/
@@ -37,33 +40,31 @@ func main() {
   
   leveldb.Write_database_leveldb(&rows, &path)
   measuretime.Measuretime_stop(start)
+   
   
-    
-  
-  /*
-  Lecture de la database
+  /*Lecture de la database*/
   fmt.Printf("Lecture BD")
   start = measuretime.Measuretime_start()
-  influxdb.Read_all_rows_database_influxdb(ip, port)
+  leveldb.Read_all_rows_database_leveldb(&path, &print)
   measuretime.Measuretime_stop(start)
   
-  Lecture de n éléments
+  /*Lecture de n éléments*/
   fmt.Printf("Lecture du nième élément")
   start = measuretime.Measuretime_start()
-  influxdb.Read_one_row_database_influxdb(lignes, ip, port) 
+  leveldb.Read_one_row_database_leveldb(nb_row_to_print, path, print) 
   measuretime.Measuretime_stop(start)
   
-  Supression du nième élément
+  /*Supression du nième élément*/
   fmt.Printf("Suppression du nième élément")
   start = measuretime.Measuretime_start()
-  influxdb.Delete_one_row_database_influxdb(lignes, ip, port) 
+  leveldb.Delete_one_row_database_leveldb(nb_row_to_delete, path) 
   measuretime.Measuretime_stop(start)
   
-  Suppresion de la database
+  /*Suppresion de la database*/
   fmt.Printf("Suppression DB")
   start = measuretime.Measuretime_start()
-  influxdb.Delete_database_influxdb(ip, port)
-  measuretime.Measuretime_stop(start)*/
+  leveldb.Delete_database_leveldb(path)
+  measuretime.Measuretime_stop(start)
   
 
 }
