266cd594671ef29869e5860193508caf6edfdfcd
Costa83 <guillaume.costagliola@gmail.com>
2020-06-05 17:38:16 +0200

    Update main.go

--- br2_bd_guillaume/package/influxdb/main.go
+++ br2_bd_guillaume/package/influxdb/main.go
@@ -1,3 +1,9 @@
+/* InfluxDB test  : Insertion de N rows */ 
+/* go mod init test_influxdb
+/ go mod tidy */
+
+
+
 package main
 
 import (
@@ -9,46 +15,58 @@ import (
 
 func main() {
 
-  /*Création de la base de donnée*/
 
+  ip := "192.168.0.10"
+  port := "8086"
+  bd := "test_influxdb_go"
+  lignes := "1"
+
+  fmt.Printf("PROGRAMME DE TEST DE INFLUXDB EN GO")
+  fmt.Printf("## Lignes : %s",lignes)
+  fmt.Printf("## Port : %s",port)
+  fmt.Printf("## Ip : %s",ip)
+  fmt.Printf("## BD : %s",bd)
+
+  
+ 
+
+  /*Création de la base de donnée*/
   start  := time.Now()
   start = measuretime.Measuretime_start()
   fmt.Printf("Création de la base de donnée sur localhost sur le port 8086")
-  influxdb.Create_database_influxdb("192.168.0.10", "8086", "test_influxdb_go")
+  influxdb.Create_database_influxdb(ip, port, bd)
   measuretime.Measuretime_stop(start)
   
-  /*Insertion de n éléments*/
-  fmt.Printf("Insertion de 11 éléments")
+  /*Insertion de n lignes*/
+  fmt.Printf("Insertion de 11 lignes")
   start = measuretime.Measuretime_start()
-  rows := "11"
-  ip := "192.168.0.10"
-  port := "8086"
-  influxdb.Write_database_influxdb(&rows, &ip, &port)
+  
+  influxdb.Write_database_influxdb(&lignes, &ip, &port)
   measuretime.Measuretime_stop(start)
   
   /*Lecture de la database*/
   fmt.Printf("Lecture BD")
   start = measuretime.Measuretime_start()
-  influxdb.Read_all_rows_database_influxdb("192.168.0.10", "8086")
+  influxdb.Read_all_rows_database_influxdb(ip, port)
   measuretime.Measuretime_stop(start)
   
   /*Lecture de n éléments*/
   fmt.Printf("Lecture du nième élément")
   start = measuretime.Measuretime_start()
-  influxdb.Read_one_row_database_influxdb("11", "192.168.0.10", "8086") 
+  influxdb.Read_one_row_database_influxdb(lignes, ip, port) 
   measuretime.Measuretime_stop(start)
   
   /*Supression du nième élément*/
   fmt.Printf("Suppression du nième élément")
   start = measuretime.Measuretime_start()
-  influxdb.Delete_one_row_database_influxdb("11", "192.168.0.10", "8086") 
+  influxdb.Delete_one_row_database_influxdb(lignes, ip, port) 
   measuretime.Measuretime_stop(start)
   
   /*Suppresion de la database*/
-  fmt.Printf("Suppression DB")
+  /*fmt.Printf("Suppression DB")
   start = measuretime.Measuretime_start()
-  influxdb.Delete_database_influxdb("192.168.0.10", "8086") 
-  measuretime.Measuretime_stop(start)
+  influxdb.Delete_database_influxdb(ip, port)
+  measuretime.Measuretime_stop(start)*/
   
 
 }
